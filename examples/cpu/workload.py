408e7089f67286a22b3e8e48f41a2a6d5f3b9fb8
NickOsipov <nick.osipov.91@gmail.com>
2024-11-20 01:10:22 +0300

    feat: cpu

---
+++ examples/cpu/workload.py
@@ -0,0 +1,44 @@
+"""
+Workload module
+"""
+
+import re
+
+from pymorphy3 import MorphAnalyzer
+
+regexp = re.compile(r"\w+")
+morph = MorphAnalyzer()
+
+
+def get_normal_form(word: str) -> str:
+    """
+    Get normal form of the word
+
+    Parameters
+    ----------
+    word : str
+        Word for getting normal form
+    
+    Returns
+    -------
+    str
+        Normal form of the word
+    """
+    return morph.parse(word)[0].normal_form
+
+
+def process_text(text: str) -> str:
+    """
+    Func for process text
+
+    Parameters
+    ----------
+    text : str
+        Text for processing
+
+    Returns
+    -------
+    str
+        Processed text
+    """
+    return " ".join(map(lambda x: get_normal_form(str(x)), regexp.findall(text)))
